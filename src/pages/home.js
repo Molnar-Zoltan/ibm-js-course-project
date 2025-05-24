753db73bee31bf196986eb5af3a0a28dc4a46aff
Molnar-Zoltan <weperdev@gmail.com>
2025-05-24 07:05:30 +0200

    Add 'Home' page

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,10 +3,19 @@
 export function Home() {
     return (
         `
-            <section>
-                <h1>EXPLORE DREAM DESTINATION</h1>
-                <p>It encourages exploration of unfamiliar territories, embracing diverse cultures and landscapes, 
-                while pursuing the desired destination that captivates the heart and ignites a sense of wonder.</p>
+            <section class="flex h-full justify-between px-10">
+                <div id="home-text" class="flex flex-col gap-4 w-150 justify-center -mt-25">
+                    <h1 class="text-7xl font-bold">EXPLORE DREAM DESTINATION</h1>
+                    <p class="text-lg">
+                        It encourages exploration of unfamiliar territories, embracing diverse cultures and landscapes, 
+                        while pursuing the desired destination that captivates the heart and ignites a sense of wonder.
+                    </p>
+                    <button type="button" id="exploreButton" class="px-2 py-1 bg-gray-200/10 rounded-lg hover:bg-gray-200/20 cursor-pointer w-40">
+                        BOOK NOW
+                    </button>
+                </div>
+                <div id="search-results" class="flex items-center justify-center">
+                </div>
             </section>
         `
     );
